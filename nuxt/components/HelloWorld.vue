8db8469ef7342bf80179c8bfd54a48f560571457
naughtLdy <naughtLdy@users.noreply.github.com>
2019-01-11 17:48:12 +0900

    add nuxt version

---
+++ nuxt/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    message: {
+      type: String,
+      default: ''
+    }
+  }
+});
+</script>
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld :message="message" />
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'Hello World'
+    };
+  }
+};
+</script>
